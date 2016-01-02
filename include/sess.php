72ed1f0cbcbb8120183bc61af0df9cb7471a9570
Qwant50 <qwantonline@gmail.com>
2016-01-02 16:36:10 +0200

    serialize

--- include/sess.php
+++ include/sess.php
@@ -5,35 +5,74 @@
  * Date: 12/25/15
  * Time: 5:43 PM
  */
-$_SESS = array();
 
-function sess_start(){
+function sess_start()
+{
 
-    global $_SESS;
+    $cookieName = ini_get('session.name');
     $path = ini_get('session.save_path');
 
-    if (isset($_COOKIE["PHPSESSID"])) {
-        $file_name = 'sess_'.$_COOKIE["PHPSESSID"];
-        $buf = file_get_contents($path.DIRECTORY_SEPARATOR.$file_name);
-        $_SESS = unserialize($buf);
-    }
-    else {
-        $id = uniqid();
-        setcookie("PHPSESSID", $id);
-        $_COOKIE["PHPSESSID"] = $id;
-
+    if (isset($_COOKIE[$cookieName])) {
+        $file_name = 'sess_' . $_COOKIE[$cookieName];
+        $buf = file_get_contents($path . DIRECTORY_SEPARATOR . $file_name);
+        sess_decode($buf);
+    } else {
+        $id = md5(uniqid(rand(),1));
+        $_COOKIE[$cookieName] = $id;
+        $GLOBALS['_SESS'] = [];
+        $file_name = 'sess_' . $_COOKIE[$cookieName];
+        file_put_contents($path . DIRECTORY_SEPARATOR . $file_name, '');
     };
-    register_shutdown_function('myShutdown');
+    register_shutdown_function('sess_write_close');
 }
 
-function myShutdown()
+function sess_write_close()
 {
-    global $_SESS;
-    if (isset($_COOKIE["PHPSESSID"])) {
-        $path = ini_get('session.save_path');
-        $file_name = 'sess_'.$_COOKIE["PHPSESSID"];
-        $buf = serialize($_SESS);
-        file_put_contents($path.DIRECTORY_SEPARATOR.$file_name, $buf);
+    $path = ini_get('session.save_path');
+    $cookieName = ini_get('session.name');
+    $file_name = 'sess_' . $_COOKIE[$cookieName];
+    file_put_contents($path . DIRECTORY_SEPARATOR . $file_name, sess_encode());
+    setcookie($cookieName, $_COOKIE[$cookieName]);
 
+}
+
+function sess_encode(){
+
+    $buf = serialize($GLOBALS['_SESS']);
+    if ($buf) {
+        preg_match('/^a:\\d+:{/', $buf, $matches);
+        $buf = substr($buf,  strlen($matches[0]), -2);
+        preg_match('/\\d+/', $matches[0], $matches);
+        $count = (int)$matches[0];
+        $matches = explode(";", $buf);
+        $encoded_string = '';
+        for ( $i = 0; $i < $count*2; $i+=2):
+            // get name
+            $format = explode(":", $matches[$i]);
+            // remove ??????? ???????
+            $format[2] = substr($format[2], 1, -1);
+            // get value
+            //$value = explode(":", $matches[$i+1]);
+            // set
+            $encoded_string .= $format[2].'|'.$matches[$i+1].';';
+        endfor;
+        return $encoded_string;
     }
+};
+
+function sess_decode($buf){
+
+    $decoded_string = '';
+    $buf = substr($buf, 0, -1);
+    $matches = explode(";", $buf);
+    foreach ($matches as $argument):
+        $seed = explode('|', $argument);
+        $decoded_string .= 's:'.strlen($seed[0]).':"'.$seed[0].'";'.$seed[1].';';
+    endforeach;
+    $decoded_string = 'a:'.count($matches).':{'.$decoded_string.'}';
+
+    $GLOBALS['_SESS'] = unserialize($decoded_string);
+    var_dump($GLOBALS['_SESS']);
+    return true;
+
 };
--- index.php
+++ index.php
@@ -9,11 +9,21 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
 //session_start();
+//sleep (60);
 
 require_once 'include/sess.php';
 
 sess_start();
 
-echo $_SESS["id"];
-$_SESS["id"] = 42;
+echo $_COOKIE['PHPSESSID'];
+//var_dump($_SESS);
 
+echo $_SESS["id"];
+$_SESS["id"] = 43;
+$_SESS["self"] = "two";
+$_SESS["test"] = 0;
+echo '<br>';
+//echo sess_encode();
+
+// my - id|i:42;self|s:3:"two";test|i:0;
+// ph - id|i:42;self|s:3:"two";test|i:0;
